d3782d79cdf75437ce27e3a813b726667bd6ca61
James Molson <james.molson@hep.manchester.ac.uk>
2010-02-25 12:28:19 +0000

    Change from tilted to normal aperture, code formatting changes for readability.

--- Merlin/Collimators/ScatterProton.h
+++ Merlin/Collimators/ScatterProton.h
@@ -4,4 +4,4 @@
 #include "AcceleratorModel/Apertures/TiltedAperture.hpp"
 
 pair<double,double> CoulombScatterp(double x, double theta0);
-int ScatterProton(PSvector& p, double x, double E0,const  TiltedAperture* tap);
+int ScatterProton(PSvector& p, double x, double E0,const  Aperture* tap);
