8d31fbdbeaee534d22a811567d6200610f659944
James Molson <james.molson@hep.manchester.ac.uk>
2010-04-20 11:37:21 +0100

    Initial MPI add.

---
+++ Merlin/MPI/merlin_mpi.hpp
@@ -0,0 +1,21 @@
+#ifndef _MERLIN_MPI_HPP_
+#define _MERLIN_MPI_HPP_
+
+#include <mpi.h>
+
+extern int size;
+extern int rank;
+extern int tag;
+extern MPI::Datatype MPI_Particle;
+extern MPI::Datatype MPI_Transport_Map;
+
+void merlin_mpi_init(int argc, char* argv[]);
+
+void merlin_worker_node();
+
+//Create a particle type for network sends - 6 doubles
+// x x' y y' ct dp
+//See BeamModel/PSvector.h
+
+
+#endif
