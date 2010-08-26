0c2a7b06a3bf21c1e9a03b8e64ac7ea5f0a2774b
James Molson <james.molson@hep.manchester.ac.uk>
2010-08-26 17:54:32 +0100

    General header to contain all particle bunch types.

---
+++ Merlin/BeamDynamics/ParticleTracking/ParticleBunchTypes.h
@@ -0,0 +1,9 @@
+#ifndef ParticleBunchTypes_h
+#define ParticleBunchTypes_h 1
+
+#include "BeamDynamics/ParticleTracking/ParticleBunch.h"
+#include "BeamDynamics/ParticleTracking/ProtonBunch.h"
+#include "BeamDynamics/ParticleTracking/ElectronBunch.h"
+#include "BeamDynamics/ParticleTracking/MuonBunch.h"
+
+#endif
