c635ccbd0515b32fd9f80f84e2a4396b1fcf9efd
Sam Tygier <sam.tygier@hep.manchester.ac.uk>
2015-08-10 15:40:22 +0100

    Add Symplectic Inegrators from Andy Wolski. Added required boilerplate.

---
+++ Merlin/BeamDynamics/ParticleTracking/Integrators/SymplecticIntegrators.h
@@ -0,0 +1,36 @@
+#ifndef SymplecticIntegrators_h
+#define SymplecticIntegrators_h 1
+
+#include "BeamDynamics/ParticleTracking/Integrators/StdIntegrators.h"
+#include "BeamDynamics/ParticleTracking/ParticleComponentTracker.h"
+
+
+namespace ParticleTracking{
+namespace SYMPLECTIC {
+
+	using namespace ParticleTracking;
+
+	DECL_SIMPLE_INTG(DriftCI,Drift)
+	DECL_SIMPLE_INTG(TWRFStructureCI,TWRFStructure)
+	DECL_SIMPLE_INTG(SWRFStructureCI,SWRFStructure)
+	DECL_SIMPLE_INTG(RectMultipoleCI,RectMultipole)
+	DECL_SIMPLE_INTG(MarkerCI,Marker)
+
+	// from std intergrators
+	DECL_SIMPLE_INTG(MonitorCI,Monitor)
+	DECL_SIMPLE_INTG(SolenoidCI,Solenoid)
+
+
+	class SectorBendCI : public ParticleComponentTracker::Integrator<SectorBend> {
+	public:
+		void TrackStep(double);
+		void TrackEntrance();
+		void TrackExit();
+	};
+	
+	
+DECL_INTG_SET(ParticleComponentTracker,StdISet)
+
+}  // end namespace SYMPLECTIC
+}
+#endif
