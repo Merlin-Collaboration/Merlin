ed06af1de0e34d411bd5b34f9783ce16a93afc9c
HaroonRafique <haroon.rafique@hud.ac.uk>
2015-11-18 16:54:13 +0000

    Added ApertureSurvey, based on Sam Tygiers function

---
+++ Merlin/AcceleratorModel/ApertureSurvey.h
@@ -0,0 +1,26 @@
+#ifndef ApertureSurvey_h
+#define ApertureSurvey_h 1
+
+#include <fstream>
+
+#include "AcceleratorModel/AcceleratorModel.h"
+#include "AcceleratorModel/Aperture.h"
+
+class ApertureSurvey
+{
+public:
+	// Constructor
+	ApertureSurvey(AcceleratorModel* model, string directory, double step_size=0.1, size_t points_per_element=0);
+	// Overloaded Constructor
+	ApertureSurvey(AcceleratorModel* model, std::ostream* os, double step_size=0.1, size_t points_per_element=0);
+	
+private:
+	void CheckAperture(Aperture* ap, double s, double *lims);
+	
+	size_t points;
+	double step_size;	
+	std::ofstream* output_file;
+	AcceleratorModel* AccMod;
+};
+
+#endif
