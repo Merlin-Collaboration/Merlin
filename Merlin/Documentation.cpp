0195652ab5cb28df2cf89344065056d5e21d5b2d
James Molson <molson@lal.in2p3.fr>
2015-12-02 19:03:27 +0100

    First commit for the new documentation. Updated doxygen input file and a new Documentation.cpp containing just a comment with the new index page.

---
+++ Merlin/Documentation.cpp
@@ -0,0 +1,37 @@
+/*! \mainpage Merlin Index
+ *
+* \section intro_sec Introduction
+*
+* This is the introduction.
+*
+* \section install_sec Installation
+*
+* \subsection Linux
+* The current method is to use cmake to build makefiles for Merlin
+* 1: make a "build" folder outside the folder where Merlin was extracted.
+* 2: run "cmake /path/to/merlin/folder"
+* 3: run "ccmake /path/to/buildfolder"
+* 4: Pick the options you wish to use.
+* 5: run make -j <ncpu>
+* 6: This will make the merlin library
+*
+* The alternate method is to use the older makefiles
+* See Merlin/merlin-unix/ 
+*
+* \subsection Windows
+* Needs to be updated.
+* \subsection OSX
+* The optimal way to use Merlin on OSX is to get cmake to generate build files for Xcode.
+* This of course requires that xcode is installed.
+* By default cmake will generate unix makefiles.
+* To generate Xcode files the appropriate generator must be specified when using cmake as:
+*
+* cmake -G Xcode
+*
+* One can then run:
+*
+* xcodebuild
+*
+* which will compile merlin.
+* Other options exist such as "xcodebuild clean", which will clear out built files.
+*/
