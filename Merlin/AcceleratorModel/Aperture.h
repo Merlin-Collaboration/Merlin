cf1f88efbcdc72d8df20248be58c5c56b999130b
James Molson <molson@lal.in2p3.fr>
2015-12-04 18:56:39 +0100

    Formatting and documentation for Aperture.

--- Merlin/AcceleratorModel/Aperture.h
+++ Merlin/AcceleratorModel/Aperture.h
@@ -28,51 +28,73 @@
 class Aperture
 {
 public:
+
+	/**
+	* Constructor
+	* @param[in] m The material to attach to this aperture.
+	*/
 	Aperture(Material* m = NULL) : ApertureMaterial(m){}
 
+	/**
+	* Destructor
+	*/
 	virtual ~Aperture ();
 
 	/**
 	* Returns true if the point (x,y,z) is within the aperture.
-	* @param x The x coordinate of the particle
-	* @param y The y coordinate of the particle
-	* @param z The z coordinate of the particle
+	* @param[in] x The x coordinate of the particle
+	* @param[in] y The y coordinate of the particle
+	* @param[in] z The z coordinate of the particle
+	* @return A bool set to true if the specified point, is within the Aperture. false if it is not.
 	*/
 	virtual bool PointInside (double x, double y, double z) const = 0;
 
 	/**
 	* Returns true if the point p is within the aperture.
-	* @param p 3D point reference to check
+	* @param[in] p 3D point reference to check
+	* @return A bool set to true if the specified point, is within the Aperture. false if it is not.
 	*/
 	bool PointInside (const Point3D& p) const;
 
 	/**
 	* Returns the radius to the aperture at location z and angle phi.
-	* @param phi The angle
-	* @param z The z posision of the particle
+	* @param[in] phi The angle.
+	* @param[in] z The z position of the particle.
+	* @return A double containing the aperture radius.
 	*/
 	virtual double GetRadiusAt (double phi, double z) const = 0;
 
 	/**
 	* Returns the type of the aperture.
+	* @return A string containing the type of the aperture.
 	*/
 	virtual string GetApertureType() const = 0;
 
-	Material* GetMaterial() const {return ApertureMaterial;}
-	void SetMaterial(Material* m){ApertureMaterial = m ;}
+	/**
+	* Gets the material associated with this aperture.
+	* @return A pointer to a Material class attached to this aperture.
+	*/
+	Material* GetMaterial() const;
+
+	/**
+	* Sets the material associated with this aperture.
+	* @param[in] m The material to attach to this aperture.
+	*/
+	void SetMaterial(Material* m);
+
+	/**
+	* Prints out the Aperture parameters to a specified stream.
+	* @param[out] out The output stream to use.
+	*/
 	virtual void printout(std::ostream& out) const;
 
 protected:
+
+	/**
+	* The Material of this aperture.
+	*/
 	Material* ApertureMaterial;
 };
-
-inline Aperture::~Aperture () {}
-
-inline bool Aperture::PointInside (const Point3D& p) const
-{
-	return PointInside(p.x,p.y,p.z);
-}
-
 
 std::ostream& operator<< (std::ostream& out, const Aperture& ap);
 
