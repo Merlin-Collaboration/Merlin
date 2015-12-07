a8f499a08b6da3902161e9e765cd4a54bd7f1063
HaroonRafique <haroon.rafique@hud.ac.uk>
2015-12-07 21:48:26 +0000

    TrackingOutputAV: supression of non scattered particles from output

--- Merlin/BeamDynamics/TrackingOutputAV.cpp
+++ Merlin/BeamDynamics/TrackingOutputAV.cpp
@@ -5,7 +5,11 @@ using namespace std;
 using namespace ParticleTracking;
 
 TrackingOutputAV::TrackingOutputAV(const std::string& filename):
+<<<<<<< HEAD
+SimulationOutput(), turn_number(0), current_s(0), current_s_set(0), single_turn(1), turn_range_set(0), s_range_set(0), suppress_unscattered(1)
+=======
 SimulationOutput(), suppress_factor(1), current_s(0), current_s_set(0), single_turn(1), turn_range_set(0), s_range_set(0), turn_number(0)
+>>>>>>> Master
 {
 	output_file = new std::ofstream(filename.c_str());
 	(*output_file) << "#id turn S x xp y yp dp type" << std::endl;
@@ -46,10 +50,16 @@ void TrackingOutputAV::Record(const ComponentFrame* frame, const Bunch* bunch)
 	double zComponent=frame->GetPosition() + frame->GetGeometryLength()/2;
 
 	const ParticleBunch* PB = static_cast<const ParticleBunch*>(bunch);
+<<<<<<< HEAD
+	for(ParticleBunch::const_iterator pb = PB->begin(); pb!= PB->end(); pb++){
+		//~ if(pb->type() != -1){
+		if((suppress_unscattered && pb->type() != -1) || (!suppress_unscattered)){
+=======
 	for(ParticleBunch::const_iterator pb = PB->begin(); pb!= PB->end(); pb++)
 	{
 		if(pb->type() != -1)
 		{
+>>>>>>> Master
 			(*output_file) 	<< int(pb->id()) << " "
 							<< turn_number << " "
 							<< std::fixed 
@@ -63,7 +73,8 @@ void TrackingOutputAV::Record(const ComponentFrame* frame, const Bunch* bunch)
 							<< pb->dp()  << " "
 							<< std::fixed
 							<< int(pb->type()) <<  " "
-							<< int(pb->id())  << endl;
+							//~ << int(pb->id())  << endl;
+							<< endl;
 		}
 	}
 
@@ -72,7 +83,12 @@ void TrackingOutputAV::Record(const ComponentFrame* frame, const Bunch* bunch)
 void TrackingOutputAV::RecordInitialBunch(const Bunch* bunch){}
 void TrackingOutputAV::RecordFinalBunch(const Bunch* bunch){}
 
+<<<<<<< HEAD
+void TrackingOutputAV::SuppressUnscattered(const bool s){
+	suppress_unscattered = s;
+=======
 void TrackingOutputAV::SuppressUnscattered(const int factor)
 {
 	suppress_factor = factor;
+>>>>>>> Master
 }
