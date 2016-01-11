a0f649a5a486e5f73621ce0934c085804ed5ed0f
James Molson <molson@lal.in2p3.fr>
2016-01-11 17:25:47 +0100

    Add CPU and system specific code, such as CPU features and NUMA access.

---
+++ Merlin/utility/CPUFeatures.h
@@ -0,0 +1,31 @@
+#ifndef _CPUFeatures_h_
+#define _CPUFeatures_h_ 1
+
+#include <iostream>
+#include <string>
+
+#ifdef LIBNUMA
+#include <numa.h>
+#endif
+
+namespace CPUFeatures
+{
+
+void CheckCPUFeatures();
+unsigned int GetCPUFeatures1();
+unsigned int GetCPUFeatures2();
+std::string GetCPUName();
+
+/**
+* NUMA features (Non Uniform Memory Access)
+* See "man numa" on linux for details
+*/
+#ifdef LIBNUMA
+bool CheckNUMA();
+void PrintNUMAInfo();
+#endif
+
+}//End namespace
+
+#endif
+
