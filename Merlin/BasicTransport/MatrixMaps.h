b886dc2fa0e44bc0a79a8bd050ad2a62884221a7
James Molson <molson@lal.in2p3.fr>
2016-03-02 15:46:48 +0100

    Box up comments for MatrixMaps.h

--- Merlin/BasicTransport/MatrixMaps.h
+++ Merlin/BasicTransport/MatrixMaps.h
@@ -24,25 +24,30 @@
 
 #include "NumericalUtils/utils.h"
 
-//	Base class for linear matrix maps. LinMtrxMap is
-//	intended only as a common base for class RMtrx and class
-//	RdpMtrx. It cannot be instantiated.
-
+/**
+* Base class for linear matrix maps. LinMtrxMap is
+* intended only as a common base for class RMtrx and class
+* RdpMtrx. It cannot be instantiated.
+*/
 class LinMtrxBase
 {
 public:
 
 	virtual ~LinMtrxBase() {};
 
-	//	Return the number of degrees of freedom for this matrix
-	//	(dimension/2).
+	/**
+	* Return the number of degrees of freedom for this matrix (dimension/2).
+	*/
 	int GetNDF () const;
 
-	//	Return the reference momentum in GeV/c (returns 0 if the
-	//	map is energy independent).
+	/**
+	* Return the reference momentum in GeV/c (returns 0 if the map is energy independent).
+	*/
 	double GetRefMomentum () const;
 
-	//	Returns true if this map is energy independent.
+	/**
+	* Returns true if this map is energy independent.
+	*/
 	bool EnergyIndependent () const;
 
 	RealMatrix R;
@@ -51,136 +56,175 @@ protected:
 
 	LinMtrxBase (double P, int ndf);
 
-	//	Construction directly from an arbitrary matrix.
+	/**
+	* Construction directly from an arbitrary matrix.
+	*/
 	LinMtrxBase (const RealMatrix& RR, double P0);
 
-	//	Returns the momentum error re-scaled to the reference
-	//	momentum.
+	/**
+	* Returns the momentum error re-scaled to the reference momentum.
+	*/
 	double scaledp (double P, double dp) const;
 
 	double Pref;
 };
 
-//	RMtrx represents a linear map which is implemented by a
-//	matrix (R matrix). The map can act on single vectors or
-//	first- and second-order moments, or arrays of the above.
-//	The dimensionality of the map is specified in the number
-//	of degrees of freedom (1,2 or 3).  In addition, RMtrx
-//	objects are associated with a specific reference
-//	momentum, about which the map (R) is calculated.
-//	Functions are provided for mapping vectors or moments
-//	whose reference momenta are different from the RMtrx
-//	reference. RMtrx objects can be multiplied together and
-//	inverted.
-
+/**
+* RMtrx represents a linear map which is implemented by a
+* matrix (R matrix). The map can act on single vectors or
+* first- and second-order moments, or arrays of the above.
+* The dimensionality of the map is specified in the number
+* of degrees of freedom (1,2 or 3).  In addition, RMtrx
+* objects are associated with a specific reference
+* momentum, about which the map (R) is calculated.
+* Functions are provided for mapping vectors or moments
+* whose reference momenta are different from the RMtrx
+* reference. RMtrx objects can be multiplied together and
+* inverted.
+*/
 class RMtrx : public LinMtrxBase
 {
 public:
 
-	//	Constructor taking the reference momentum and the number
-	//	of degrees of freedom (ndf) for the matrix. The  matrix
-	//	is dimensioned but not initialised.
+	/**
+	* Constructor taking the reference momentum and the number
+	* of degrees of freedom (ndf) for the matrix. The  matrix
+	* is dimensioned but not initialised.
+	*/
 	explicit RMtrx (int ndf = 3, double P = 0);
 
-	//	Construction from an aribrary matrix. The matrix should
-	//	be square with dimension 2n, where n is 1,2 or 3.
+	/**
+	* Construction from an aribrary matrix. The matrix should
+	* be square with dimension 2n, where n is 1,2 or 3.
+	*/
 	RMtrx (const RealMatrix& RR, double P0 = 0);
 
-	// Set the reference momentum
+	/**
+	* Set the reference momentum
+	*/
 	void SetRefMomentum(double p)
 	{
 		Pref = p;
 	}
 
-	//	Transforms the vector x by R such that x->R.x. The
-	//	second form explicitly specifies the reference momentum
-	//	(GeV/c) for the vector. Returns x.
+	/**
+	* Transforms the vector x by R such that x->R.x. The
+	* second form explicitly specifies the reference momentum
+	* (GeV/c) for the vector. Returns x.
+	*/
 	PSvector& Apply (PSvector& x) const;
 	PSvector& Apply (PSvector& x, double p0) const;
 
-	//	Transforms each vector in xa by R such that x->R.x. The
-	//	second form explicitly specifies the reference momentum
-	//	(GeV/c) for the vectors. Returns xa.
+	/**
+	* Transforms each vector in xa by R such that x->R.x. The
+	* second form explicitly specifies the reference momentum
+	* (GeV/c) for the vectors. Returns xa.
+	*/
 	PSvectorArray& Apply (PSvectorArray& xa) const;
 	PSvectorArray& Apply (PSvectorArray& xa, double p0) const;
 
-	//	Transform  sigma by R. If X,S represent the first- and
-	//	second-order moments respectively, then X->R.X and
-	//	S->R.S.R'. The second form explicitly specifies the
-	//	reference momentum (GeV/c)  for the moments. Returns
-	//	sigma.
+	/**
+	* Transform  sigma by R. If X,S represent the first- and
+	* second-order moments respectively, then X->R.X and
+	* S->R.S.R'. The second form explicitly specifies the
+	* reference momentum (GeV/c)  for the moments. Returns
+	* sigma.
+	*/
 	PSmoments& Apply (PSmoments& sigma) const;
 	PSmoments& Apply (PSmoments& sigma, double p0) const;
 
-	//	Transforms the moments in sigmaArray by R (see
-	//	apply(PSmoments&) for details). The second form
-	//	explicitly specifies the reference momentum (GeV/c) for
-	//	the moments. Returns sigmaArray.
+	/**
+	* Transforms the moments in sigmaArray by R (see
+	* apply(PSmoments&) for details). The second form
+	* explicitly specifies the reference momentum (GeV/c) for
+	* the moments. Returns sigmaArray.
+	*/
 	PSmomentsArray& Apply (PSmomentsArray& sigmaArray) const;
 	PSmomentsArray& Apply (PSmomentsArray& sigmaArray, double p0) const;
 
-	//	Invert the matrix.
+	/**
+	* Invert the matrix.
+	*/
 	RMtrx& Invert ();
 
-	//	Dot operation. (*this)*=R represents (*this)->R.(*this).
+	/**
+	* Dot operation. (*this)*=R represents (*this)->R.(*this).
+	*/
 	RMtrx& operator *= (const RMtrx& rhs);
 
-	// array-like accessor
+	/**
+	* array-like accessor
+	*/
 	double operator()(int i, int j) const
 	{
 		return R(i-1,j-1);
 	}
 };
 
-//	An approximate linear map which contains a first-order
-//	chromatic correction. The matrix used for tracking is
-//	calculated as R0+R1*dp, where R0 is the matrix
-//	calculated about the reference momentum, R1 is the first
-//	order derivative wrt momentum and dp is the momentum
-//	error. For more information see RMtrx.
-
+/**
+* An approximate linear map which contains a first-order
+* chromatic correction. The matrix used for tracking is
+* calculated as R0+R1*dp, where R0 is the matrix
+* calculated about the reference momentum, R1 is the first
+* order derivative wrt momentum and dp is the momentum
+* error. For more information see RMtrx.
+*/
 class RdpMtrx : public LinMtrxBase
 {
 public:
 
-	//	Constructor taking the reference momentum and the number
-	//	of degrees of freedom (ndf) for the matrix. The
-	//	matrices are dimensioned but not initialised.
+	/**
+	* Constructor taking the reference momentum and the number
+	* of degrees of freedom (ndf) for the matrix. The
+	* matrices are dimensioned but not initialised.
+	*/
 	explicit RdpMtrx (int ndf = 3, double P0 = 0);
 
-	//	Constructor from two arbitrary matrices. Both matrices
-	//	should be square and of equal dimension, which should be
-	//	2n where n=1,2, or 3.
+	/**
+	* Constructor from two arbitrary matrices. Both matrices
+	* should be square and of equal dimension, which should be
+	* 2n where n=1,2, or 3.
+	*/
 	RdpMtrx (const RealMatrix& RR, const RealMatrix& TT, double P0 = 0);
 
-	//	Transforms the vector x by R such that x->R.x. The
-	//	second form explicitly specifies the reference momentum
-	//	(GeV/c) for the vector. Returns x.
+	/**
+	* Transforms the vector x by R such that x->R.x. The
+	* second form explicitly specifies the reference momentum
+	* (GeV/c) for the vector. Returns x.
+	*/
 	PSvector& Apply (PSvector& x) const;
 	PSvector& Apply (PSvector& x, double p0) const;
 
-	//	Transforms each vector in xa by R such that x->R.x. The
-	//	second form explicitly specifies the reference momentum
-	//	(GeV/c) for the vectors. Returns xa.
+	/**
+	* Transforms each vector in xa by R such that x->R.x. The
+	* second form explicitly specifies the reference momentum
+	* (GeV/c) for the vectors. Returns xa.
+	*/
 	PSvectorArray& Apply (PSvectorArray& xa) const;
 	PSvectorArray& Apply (PSvectorArray& xa, double p0) const;
 
-	//	Transform  sigma by R. If X,S represent the first- and
-	//	second-order moments respectively, then X->R.X and
-	//	S->R.S.R'. The second form explicitly specifies the
-	//	reference momentum (GeV/c)  for the moments. Returns
-	//	sigma.
+	/**
+	* Transform  sigma by R. If X,S represent the first- and
+	* second-order moments respectively, then X->R.X and
+	* S->R.S.R'. The second form explicitly specifies the
+	* reference momentum (GeV/c)  for the moments. Returns
+	* sigma.
+	*/
 	PSmoments& Apply (PSmoments& sigma) const;
 	PSmoments& Apply (PSmoments& sigma, double p0) const;
 
-	//	Transforms the moments in sigmaArray by R (see
-	//	apply(PSmoments&) for details). The second form
-	//	explicitly specifies the reference momentum (GeV/c) for
-	//	the moments. Returns sigmaArray.
+	/**
+	* Transforms the moments in sigmaArray by R (see
+	* apply(PSmoments&) for details). The second form
+	* explicitly specifies the reference momentum (GeV/c) for
+	* the moments. Returns sigmaArray.
+	*/
 	PSmomentsArray& Apply (PSmomentsArray& sigmaArray) const;
 	PSmomentsArray& Apply (PSmomentsArray& sigmaArray, double p0) const;
 
-	//	Second-order momemtum derivative matrix (T matrix).
+	/**
+	* Second-order momemtum derivative matrix (T matrix).
+	*/
 	RealMatrix T;
 };
 
