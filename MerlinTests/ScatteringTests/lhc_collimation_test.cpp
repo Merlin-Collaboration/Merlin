9e864775eb7808c45eea18799e6de47bfa75fa17
Sam Tygier <sam.tygier@manchester.ac.uk>
2017-01-05 14:22:14 +0000

    Convert lhc_collimation_test to using Chi^2
    Move to using Chi^2 for checking goodness of distribution. See
    5463d01ae634b1ae871b982bfb2485e2025ac748 for info. Also update data
    files.

--- MerlinTests/ScatteringTests/lhc_collimation_test.cpp
+++ MerlinTests/ScatteringTests/lhc_collimation_test.cpp
@@ -23,6 +23,8 @@
 #include "Collimators/MaterialDatabase.h"
 #include "Collimators/ApertureConfiguration.h"
 #include "RingDynamics/Dispersion.h"
+#include "AcceleratorModel/Apertures/CollimatorAperture.h"
+#include "Collimators/Output/LossMapCollimationOutput.h"
 
 using namespace std;
 using namespace PhysicalUnits;
@@ -88,23 +90,28 @@ int FindElementLatticePosition(string RequestedElement, AcceleratorModel* model)
 
 int main(int argc, char* argv[])
 {
-	int seed = (int)time(NULL);
+	int seed = 0;
 	int npart = 10000;
 	//~ int npart = 100000;
-	int nturns = 200;
+	int nturns = 20;
 
 	//Loss_Map or Merged Collimation
 	bool Loss_Map 				= 0;
-	bool st_scatter				= 1;
+	bool st_scatter				= 0;
 
 	if (argc >=2)
 	{
-		npart = atoi(argv[1]);
+		seed = atoi(argv[1]);
+	}
+
+	if (seed == 0)
+	{
+		seed = (int)time(NULL);
 	}
 
 	if (argc >=3)
 	{
-		seed = atoi(argv[2]);
+		npart = atoi(argv[2]);
 	}
 
 	cout << "Random Seed: " << seed << endl;
@@ -290,6 +297,28 @@ int main(int argc, char* argv[])
 		break;
 	}
 
+	vector<Collimator*> TCP;
+	int siz = model->ExtractTypedElements(TCP,start_element);
+	Aperture *ap = (TCP[0])->GetAperture();
+	if(!ap)
+	{
+		cout << "Could not get tcp ap" << endl;
+		abort();
+	}
+	CollimatorAperture* CollimatorJaw = dynamic_cast<CollimatorAperture*>(ap);
+	if(!CollimatorJaw)
+	{
+		cout << "Could not cast" << endl;
+		abort();
+	}
+
+	double h_offset = twiss->Value(1,0,0,start_element_number);
+	double JawPosition = CollimatorJaw->GetFullWidth() / 2.0;
+	HorizontalHaloParticleBunchFilter *hFilter = new HorizontalHaloParticleBunchFilter();
+	hFilter->SetHorizontalLimit(JawPosition);
+	hFilter->SetHorizontalOrbit(h_offset);
+	constructor->SetFilter(hFilter);
+
 	myBunch = constructor->ConstructParticleBunch<ProtonBunch>();
 	delete constructor;
 
@@ -310,85 +339,76 @@ int main(int argc, char* argv[])
 	tracker->SetLogStream(std::cout);
 
 	//	COLLIMATION SETTINGS
+	std::ostringstream filename;
+	filename << "lhc_collimation_test_lossmap_"<< npart << ".dat";
 
-	//Output stream for collimator losses
-	ostringstream col_output_file;
-	if(Loss_Map && st_scatter)
+	LossMapCollimationOutput* myLossOutput = new LossMapCollimationOutput(tencm);
+
+	if(Loss_Map)
 	{
-		col_output_file << "Bunch/LHC_Plotting/lm_st/Loss.txt";
-	}
-	else if(Loss_Map && !st_scatter)
-	{
-		col_output_file << "Bunch/LHC_Plotting/lm_m/Loss.txt";
-	}
-	else if (!Loss_Map && st_scatter)
-	{
-		col_output_file << "Bunch/LHC_Plotting/m_m/Loss.txt";
+		CollimateParticleProcess* myCollimateProcess;
+
+		myCollimateProcess=new CollimateParticleProcess(2,4);
+		myCollimateProcess->ScatterAtCollimator(true);
+
+		myCollimateProcess->SetLossThreshold(200.0);
+		myCollimateProcess->SetOutputBinSize(0.1);
+		myCollimateProcess->SetCollimationOutput(myLossOutput);
+		tracker->AddProcess(myCollimateProcess);
 	}
 	else
 	{
-		col_output_file << "Bunch/LHC_Plotting/m_st/Loss.txt";
-	}
-	if (1)  // disable tracking
-	{
-		ofstream* col_output = new ofstream(col_output_file.str().c_str());
-		if(!col_output->good())
+		CollimateProtonProcess* myCollimateProcess;
+
+		myCollimateProcess=new CollimateProtonProcess(2,4);
+		myCollimateProcess->ScatterAtCollimator(true);
+
+		ScatteringModel* myScatter = new ScatteringModel;
+		if(st_scatter)
 		{
-			std::cerr << "Could not open collimation loss file" << std::endl;
-			exit(EXIT_FAILURE);
-		}
-
-		if(Loss_Map)
-		{
-			CollimateParticleProcess* myCollimateProcess;
-
-			myCollimateProcess=new CollimateParticleProcess(2,4,col_output);
-			myCollimateProcess->ScatterAtCollimator(true);
-
-			myCollimateProcess->SetLossThreshold(200.0);
-			myCollimateProcess->SetOutputBinSize(0.1);
-			tracker->AddProcess(myCollimateProcess);
+			myScatter->SetScatterType(0);
 		}
 		else
 		{
-			CollimateProtonProcess* myCollimateProcess;
-
-			myCollimateProcess=new CollimateProtonProcess(2,4,col_output);
-			myCollimateProcess->ScatterAtCollimator(true);
-
-			ScatteringModel* myScatter = new ScatteringModel;
-			if(st_scatter)
-			{
-				myScatter->SetScatterType(0);
-			}
-			else
-			{
-				myScatter->SetScatterType(4);
-			}
-
-			myCollimateProcess->SetScatteringModel(myScatter);
-
-			myCollimateProcess->SetLossThreshold(200.0);
-			myCollimateProcess->SetOutputBinSize(0.1);
-			tracker->AddProcess(myCollimateProcess);
-
+			myScatter->SetScatterType(4);
 		}
 
-		//	TRACKING RUN
-		for (int turn=1; turn<=nturns; turn++)
-		{
-			cout << "Turn " << turn <<"\tParticle number: " << myBunch->size() << endl;
-			tracker->Track(myBunch);
-			if(myBunch->size() <= 1)
-			{
-				break;
-			}
+		myCollimateProcess->SetScatteringModel(myScatter);
 
-		}
-		col_output->flush();
-		col_output->close();
-		delete col_output;
+		myCollimateProcess->SetLossThreshold(200.0);
+		myCollimateProcess->SetOutputBinSize(0.1);
+		myCollimateProcess->SetCollimationOutput(myLossOutput);
+		tracker->AddProcess(myCollimateProcess);
 	}
+
+	//	TRACKING RUN
+	for (int turn=1; turn<=nturns; turn++)
+	{
+		cout << "Turn " << turn <<"\tParticle number: " << myBunch->size() << endl;
+		tracker->Track(myBunch);
+		if(myBunch->size() <= 1)
+		{
+			break;
+		}
+	}
+
+	myLossOutput->Finalise();
+
+
+	ofstream* col_output = new ofstream(filename.str().c_str());
+	if(!col_output->good())
+	{
+		std::cerr << "Could not open collimation loss file" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	*col_output << "# From merlin npart="<< npart << " nturns="<< nturns <<endl;
+	myLossOutput->Output(col_output);
+
+	col_output->flush();
+	col_output->close();
+	cout << "Wrote: " << filename.str() << endl;
+	delete col_output;
+
 
 	cout << "npart: " << npart << endl;
 	cout << "left: " << myBunch->size() << endl;
@@ -399,122 +419,4 @@ int main(int argc, char* argv[])
 	delete twiss;
 	delete model;
 	delete myMADinterface;
-
-	// read losses back in
-	ifstream* col_output2 = new ifstream(col_output_file.str().c_str());
-
-	set<string> all_loss_elems;
-	map<string, int> loss_map;
-	string el_name, el_pos, el_spos, hit_id;
-	double el_len, el_hits;
-
-	while (col_output2->good())
-	{
-		*col_output2 >> el_name >> el_pos >> el_len >> el_hits >> el_spos;
-		hit_id = el_name + "_" + el_pos;
-		//if(!loss_map.count(hit_id)){ loss_map[hit_id] = 0;}
-		//cout << "~" << hit_id << " " <<  el_hits << endl;
-		loss_map[hit_id] += el_hits;
-		all_loss_elems.insert(hit_id);
-	}
-
-	// load test data
-	ostringstream nom_input_file;
-	nom_input_file << input_data_dir << "/lhc_collimation_test_7tev_nom.dat";
-
-	el_hits = 7;
-	map<string, double> loss_map_nom;
-	ifstream* nom_lossmap_in = new ifstream(nom_input_file.str().c_str());
-	string line;
-	while (getline(*nom_lossmap_in, line))
-	{
-		if (line == "")
-		{
-			continue;
-		}
-		if (line[0] == '#')
-		{
-			continue;
-		}
-		istringstream liness(line);
-		liness >> hit_id >> el_hits;
-		//cout << "@" << hit_id << " " <<el_hits*npart << endl;;
-		//if(!loss_map_nom.count(hit_id)){ loss_map_nom[hit_id] = 0;}
-		loss_map_nom[hit_id] += el_hits * npart;
-		all_loss_elems.insert(hit_id);
-	}
-
-	// Calculate sigma from sqrt(<n>)
-	// Count number of bins out by more than n-sigma
-	const int ns = 4;
-	// https://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule, could use erf() in c++11
-	const double tails[] = {0, 0.682689492137086, 0.954499736103642, 0.997300203936740,
-	                        0.999936657516334, 0.999999426696856, 0.999999998026825
-	                       };
-	int over_ns[ns+1] = {0};
-	int nbins = 0;
-
-	cout << "**" << endl;
-	double sum_x1 = 0, sum_x2 = 0;
-	for(set<string>::iterator it=all_loss_elems.begin(); it!=all_loss_elems.end(); ++it)
-	{
-		double x1 = loss_map[*it];
-		double x2 = loss_map_nom[*it];
-		sum_x1 += x1;
-		sum_x2 += x2;
-
-		// small bins throw the result, so ignore them until proper poisson stats implemented FIXME
-		if (x2 < 5)
-		{
-			continue;
-		}
-		nbins++;
-
-		//double exp_err_x = max(sqrt(x2), 0.5);
-		double exp_err_x = sqrt(x2);
-		double diff_x = x1 - x2;
-
-		for(int j=1; j<ns+1; j++)
-		{
-			if (fabs(diff_x)/exp_err_x > j)
-			{
-				over_ns[j]++;
-				if (j>=2)
-				{
-					cout << "big error in x # bin, merlin, expected, error/sigma" << endl;
-					cout << *it << " " << x1 << " " << x2 << "+-" << exp_err_x << " " << diff_x/exp_err_x << endl;
-				}
-			}
-		}
-		//cout << *it << " " << loss_map[*it] << " " << loss_map_nom[*it] << endl;
-	}
-	cout << "Number of particles used "<< npart << endl;
-
-	cout << "Number of bins with sufficient expected losses "<<nbins << endl;
-
-	if (npart < 10000)
-	{
-		cout << "Warning low statistics, it is recommended to use more than 10000 particles" << endl;
-	}
-
-	// put limit on fraction of bins out by more than n-sigma
-	// bad if 30% more than expected
-
-	bool bad = 0;
-	double tol = 1.3;
-	//~ cout << endl << "Bins out by more than";
-	//~ for(int j=1; j<ns+1; j++){
-	//~ cout << endl << "    "<< j <<" sigma: "<< over_ns[j] << " ("<< (double)over_ns[j]/(nbins+1)*100  << " %)";
-	//~ if ((double)over_ns[j]/(nbins+1) > (1-tails[j])*tol) {cout << "** bad **"; bad=1;}
-	//~ }
-	//~ cout << endl << endl;
-//~
-	//~ cout << "Total losses " << sum_x1 << "  expected " << sum_x2 << endl;
-	//~ double sum_dev = fabs(sum_x1-sum_x2) / sqrt(sum_x2);
-	//~ cout << sum_dev <<  " sig deviation";
-	//~ if (sum_dev > 2) {bad += 1; cout << " ** bad **";}
-	//~ cout << endl;
-
-
-	return bad;
 }
