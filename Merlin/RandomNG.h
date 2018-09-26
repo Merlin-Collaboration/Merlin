244ea24e2267ec5ee327bfb966ce97b0458c7a4a
Sam Tygier <sam.tygier@manchester.ac.uk>
2018-09-26 16:37:41 +0100

    Remove RandGenerator class
    No longer used.

--- Merlin/RandomNG.h
+++ Merlin/RandomNG.h
@@ -9,100 +9,11 @@
 #define RandomNG_h 1
 
 #include "merlin_config.h"
-#include <cassert>
 #include <random>
 #include <memory>
 #include <cstdint>
 #include <iostream>
 #include <unordered_map>
-
-class ACG;
-class Normal;
-class Uniform;
-class Poisson;
-class Landau;
-
-/**
- * A class which represents a single random number
- * generator.
- */
-class RandGenerator
-{
-public:
-
-	RandGenerator(unsigned iseed = 0);
-	~RandGenerator();
-
-	unsigned  getSeed() const;
-
-	/**
-	 * Resets the seed for the generators to the last supplied
-	 * seed value.
-	 */
-	void reset();
-
-	/**
-	 * Resets the initial seed for the generator.
-	 */
-	void reset(unsigned iseed);
-
-	/**
-	 * Generates a random number from a uniform (Gaussian)
-	 * distribution with the specified mean and variance.
-	 */
-	double normal(double mean, double variance);
-
-	/**
-	 * Generates a random number from a uniform (Gaussian)
-	 * distribution with the specified mean and variance. The
-	 * resulting distribution is truncated to +/-cutoff
-	 * standard deviations.
-	 */
-	double normal(double mean, double variance, double cutoff);
-
-	/**
-	 * Generates a uniform random number in the range
-	 * |low,high> inclusive.
-	 */
-	double uniform(double low, double high);
-
-	/**
-	 * Generates a Poisson random number.
-	 */
-	double poisson(double u);
-
-	/**
-	 * Generates a landau random number.
-	 */
-	double landau();
-
-	/**
-	 * Initialised the generator. Should be called before any
-	 * other generator function.
-	 */
-	void init(unsigned iseed = 0);
-
-private:
-
-	unsigned nseed;
-
-	ACG* gen;
-	Normal* gaussGen;
-	Uniform* uniformGen;
-	Poisson* poissonGen;
-	Landau* landauGen;
-
-	void ResetGenerators();
-
-	RandGenerator(const RandGenerator& rand);
-	RandGenerator& operator=(const RandGenerator& rand);
-
-};
-
-inline unsigned RandGenerator::getSeed() const
-{
-	return nseed;
-}
 
 /**
  * Singleton class for generating continuous floating point numbers from specific distributions.
