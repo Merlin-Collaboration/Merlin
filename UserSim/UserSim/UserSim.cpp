6143923ee4972c9bc49d14dc5454bda28a767da4
Scott Rowan <s.rowan@hud.ac.uk>
2019-03-03 21:22:43 +0000

    activated and included UserSim

---
+++ UserSim/UserSim/UserSim.cpp
@@ -0,0 +1,23 @@
+/*
+ * Merlin++: C++ Class Library for Charged Particle Accelerator Simulations
+ * Copyright (c) 2001-2018 The Merlin++ developers
+ * This file is covered by the terms the GNU GPL version 2, or (at your option) any later version, see the file COPYING
+ * This file is derived from software bearing the copyright notice in merlin4_copyright.txt
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Welcome to Merlin++!\n" << endl;
+	cout << "Current Merlin++ version: 5.02" << endl;
+	cout << "To confirm a correct installation please run 'make test' and ensure all tests pass." << endl;
+	cout << "For more information on how to get started, please refer to MerlinDocumentation/QuickStartGuide.pdf" << endl;
+
+	/*
+	 * ENTER CODE HERE
+	 */
+
+	return 0;
+}
