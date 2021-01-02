4daffd10e44bbd4cd77bbca30b0c64609e967f46
Sam Tygier <sam.tygier@manchester.ac.uk>
2021-01-02 12:42:01 +0000

    Simplify and optimise aperture loading
    Find the aperture by binary rather than linear search. Saves ~6 seconds
    loading LHC aperture.
    
    Also make use of range based for loops.

--- Merlin++/ApertureConfiguration.cpp
+++ Merlin++/ApertureConfiguration.cpp
@@ -29,7 +29,6 @@ ApertureConfiguration::ApertureConfiguration() :
 ApertureConfiguration::ApertureConfiguration(string InputFileName) :
 	log(nullptr), logFlag(false)
 {
-	//unique_ptr<DataTable> dt(DataTableReaderTFS(InputFileName).Read());
 	auto dt = make_unique<DataTable>(DataTableReaderTFS(InputFileName).Read());
 
 	ApertureDataTable.AddColumn("S", 'd');
@@ -64,236 +63,243 @@ void ApertureConfiguration::ConfigureElementApertures(AcceleratorModel* Model)
 
 	Aperture* aperture;
 
-	for(std::vector<AcceleratorComponent*>::iterator comp = Elements.begin(); comp != Elements.end(); ++comp)
+	for(auto &comp :Elements)
 	{
-		if((*comp)->GetAperture() != nullptr)
+		if(comp->GetAperture() != nullptr)
 			continue;
-		if((*comp)->GetLength() == 0)
+		if(comp->GetLength() == 0)
 			continue;
-		double ElementLength = (*comp)->GetLength();
-		double Position = (*comp)->GetComponentLatticePosition();
-		if((*comp)->GetType() != "Drift")
+		double ElementLength = comp->GetLength();
+		double Position = comp->GetComponentLatticePosition();
+		if(comp->GetType() != "Drift")
 		{
-			for(DataTableRowIterator dt = ApertureDataTable.begin(); dt != ApertureDataTable.end(); ++dt)
-			{
-				if(dt->Get_d("S") >= Position)
-				{
-					aperture = ApertureFactory::GetInstance(*dt);
-					(*comp)->SetAperture(aperture);
-					++noConfiguredApertures;
-					break;
-				}
-			}
+			auto this_ap = std::lower_bound(ApertureDataTable.begin(), ApertureDataTable.end(), Position,
+				[](const auto a, const auto b){
+				return a.Get_d("S") < b;
+			});
+
+			aperture = ApertureFactory::GetInstance(*this_ap);
+			comp->SetAperture(aperture);
+			++noConfiguredApertures;
 		}
 		else
 		{
-			for(DataTableRowIterator dt = ApertureDataTable.begin(); dt != ApertureDataTable.end(); ++dt)
+			auto this_ap = std::lower_bound(ApertureDataTable.begin(), ApertureDataTable.end(), Position,
+				[](const auto a, const auto b){
+				return a.Get_d("S") < b;
+			});
+
+			if(this_ap == ApertureDataTable.end())
 			{
-				auto temp = ApertureDataTable.end();
-				--temp;
-				if(dt->Get_d("S") >= Position || dt == temp)
+				--this_ap;
+			}
+
+			auto last_ap = ApertureDataTable.end() - 1;
+
+			while(this_ap != ApertureDataTable.end())
+			{
+				size_t row = 0;
+				DataTable ThisElementAperture;
+				DataTable CleanElementAperture;
+				ThisElementAperture.AddColumn("APERTYPE", 's');
+				ThisElementAperture.AddColumn("S", 'd');
+				ThisElementAperture.AddColumn("APER_1", 'd');
+				ThisElementAperture.AddColumn("APER_2", 'd');
+				ThisElementAperture.AddColumn("APER_3", 'd');
+				ThisElementAperture.AddColumn("APER_4", 'd');
+
+				CleanElementAperture = ThisElementAperture;
+
+				if(this_ap == ApertureDataTable.begin())
 				{
-					size_t row = 0;
-					DataTable ThisElementAperture;
-					DataTable CleanElementAperture;
-					ThisElementAperture.AddColumn("APERTYPE", 's');
-					ThisElementAperture.AddColumn("S", 'd');
-					ThisElementAperture.AddColumn("APER_1", 'd');
-					ThisElementAperture.AddColumn("APER_2", 'd');
-					ThisElementAperture.AddColumn("APER_3", 'd');
-					ThisElementAperture.AddColumn("APER_4", 'd');
-
-					CleanElementAperture = ThisElementAperture;
-
-					if(dt == ApertureDataTable.begin())
+					//get initial point, interpolate from last point
+					ThisElementAperture.ApertureFromRow(*last_ap, row);
+					++row;
+				}
+				else
+				{
+					--this_ap;
+					ThisElementAperture.ApertureFromRow(*this_ap, row);
+					++this_ap;
+				}
+				while((*this_ap).Get_d("S") <= (Position + ElementLength))
+				{
+					++row;
+					ThisElementAperture.ApertureFromRow(*this_ap, row);
+					++this_ap;
+					if(this_ap == ApertureDataTable.end())
 					{
-						//get initial point, interpolate from last point
-						ThisElementAperture.ApertureFromRow(*temp, row);
-						++row;
+						break;
 					}
-					else
+				}
+				if(this_ap == ApertureDataTable.end())
+				{
+					++row;
+					this_ap = ApertureDataTable.begin();
+					ThisElementAperture.ApertureFromRow(*this_ap, row);
+				}
+				else
+				{
+					++row;
+					ThisElementAperture.ApertureFromRow(*this_ap, row);
+					this_ap = ApertureDataTable.end();
+				}
+
+				//First do a little bit of cleaning
+				//If we have an entry at 0 (or very close to), and also an entry at negative values, we can discard the negative entry
+				size_t thisit = 0;
+				size_t cleanit = 0;
+				bool zeroentry = false;
+				size_t negcount = 0;
+
+				for(const auto itr : ThisElementAperture)
+				{
+					if(itr == *ThisElementAperture.begin())
 					{
-						--dt;
-						ThisElementAperture.ApertureFromRow(*dt, row);
-						++dt;
-					}
-					while((*dt).Get_d("S") <= (Position + ElementLength))
-					{
-						++row;
-						ThisElementAperture.ApertureFromRow(*dt, row);
-						++dt;
-						if(dt == ApertureDataTable.end())
+						for(size_t row = 0; row < ThisElementAperture.Length(); ++row)
 						{
-							break;
-						}
-					}
-					if(dt == ApertureDataTable.end())
-					{
-						++row;
-						dt = ApertureDataTable.begin();
-						ThisElementAperture.ApertureFromRow(*dt, row);
-					}
-					else
-					{
-						++row;
-						ThisElementAperture.ApertureFromRow(*dt, row);
-						dt = ApertureDataTable.end();
-					}
-
-					//First do a little bit of cleaning
-					//If we have an entry at 0 (or very close to), and also an entry at negative values, we can discard the negative entry
-					size_t thisit = 0;
-					size_t cleanit = 0;
-					bool zeroentry = false;
-					size_t negcount = 0;
-					for(DataTableRowIterator itr = ThisElementAperture.begin(); itr != ThisElementAperture.end(); ++itr)
-					{
-						if(itr == ThisElementAperture.begin())
-						{
-							for(size_t row = 0; row < ThisElementAperture.Length(); ++row)
+							if(ThisElementAperture.Get_d("S", row) - Position < 0)
 							{
-								if(ThisElementAperture.Get_d("S", row) - Position < 0)
-								{
-									++negcount;
-								}
-								if(fequal(ThisElementAperture.Get_d("S", row) - Position, 0.0, 1e-7))
-								{
-									ThisElementAperture.Set("S", row, Position);
-									zeroentry = true;
-								}
+								++negcount;
+							}
+							if(fequal(ThisElementAperture.Get_d("S", row) - Position, 0.0, 1e-7))
+							{
+								ThisElementAperture.Set("S", row, Position);
+								zeroentry = true;
 							}
 						}
-						if(negcount != 0 && zeroentry == true)
-						{
-							--negcount;
-							continue;
-						}
-						if(negcount > 1)
-						{
-							--negcount;
-							continue;
-						}
-						CleanElementAperture.ApertureFromRow(*itr, cleanit);
-						++cleanit;
 					}
+					if(negcount != 0 && zeroentry == true)
+					{
+						--negcount;
+						continue;
+					}
+					if(negcount > 1)
+					{
+						--negcount;
+						continue;
+					}
+					CleanElementAperture.ApertureFromRow(itr, cleanit);
+					++cleanit;
+				}
 
-					if(fequal(CleanElementAperture.Get_d("S", 0) - Position, 0.0, 5e-7))
-					{
-						CleanElementAperture.Set("S", 0, Position);
-					}
+				if(fequal(CleanElementAperture.Get_d("S", 0) - Position, 0.0, 5e-7))
+				{
+					CleanElementAperture.Set("S", 0, Position);
+				}
 
-					//Determine need to interpolate
-					bool interpolate = false;
-					bool typeChange = false;
-					for(auto &itr :CleanElementAperture)
+				//Determine need to interpolate
+				bool interpolate = false;
+				bool typeChange = false;
+				for(auto &itr :CleanElementAperture)
+				{
+					if(CleanElementAperture.begin()->Get_d("APER_1") != itr.Get_d("APER_1")
+						|| CleanElementAperture.begin()->Get_d("APER_2") != itr.Get_d("APER_2")
+						|| CleanElementAperture.begin()->Get_d("APER_3") != itr.Get_d("APER_3")
+						|| CleanElementAperture.begin()->Get_d("APER_4") != itr.Get_d("APER_4"))
 					{
-						if(CleanElementAperture.begin()->Get_d("APER_1") != itr.Get_d("APER_1")
-							|| CleanElementAperture.begin()->Get_d("APER_2") != itr.Get_d("APER_2")
-							|| CleanElementAperture.begin()->Get_d("APER_3") != itr.Get_d("APER_3")
-							|| CleanElementAperture.begin()->Get_d("APER_4") != itr.Get_d("APER_4"))
-						{
-							interpolate = true;
-						}
-						if(typeChange == false && CleanElementAperture.begin()->Get_s("APERTYPE") != itr.Get_s(
-								"APERTYPE"))
-						{
-							typeChange = true;
-						}
+						interpolate = true;
 					}
-					if(interpolate)
+					if(typeChange == false && CleanElementAperture.begin()->Get_s("APERTYPE") != itr.Get_s(
+							"APERTYPE"))
 					{
+						typeChange = true;
+					}
+				}
+				if(interpolate)
+				{
+					for(size_t row = 0; row < CleanElementAperture.Length(); ++row)
+					{
+						CleanElementAperture.Set_d("S", row, (CleanElementAperture.Get_d("S", row) - Position));
+					}
+					if(typeChange == false)
+					{
+						aperture = InterpolatorFactory::GetInstance(CleanElementAperture);
+						comp->SetAperture(aperture);
+						++noConfiguredApertures;
+						break;
+					}
+					else
+					{
+						bool rectellipse = false;
+						bool octagon = false;
 						for(size_t row = 0; row < CleanElementAperture.Length(); ++row)
 						{
-							CleanElementAperture.Set_d("S", row, (CleanElementAperture.Get_d("S", row) - Position));
-						}
-						if(typeChange == false)
-						{
-							aperture = InterpolatorFactory::GetInstance(CleanElementAperture);
-							(*comp)->SetAperture(aperture);
-							++noConfiguredApertures;
-							break;
-						}
-						else
-						{
-							bool rectellipse = false;
-							bool octagon = false;
-							for(size_t row = 0; row < CleanElementAperture.Length(); ++row)
+							//CHANGE ZERO VALUE EXCEPTIONS
+							if(CleanElementAperture.Get_s("APERTYPE", row) == "RECTELLIPSE")
 							{
-								//CHANGE ZERO VALUE EXCEPTIONS
-								if(CleanElementAperture.Get_s("APERTYPE", row) == "RECTELLIPSE")
-								{
-									CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_2", row));
-									CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_3", row));
-									CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_4", row));
-									rectellipse = true;
-								}
-								if(CleanElementAperture.Get_s("APERTYPE", row) == "CIRCLE")
-								{
-									CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_1", row));
-									rectellipse = true;
-								}
-								if(CleanElementAperture.Get_s("APERTYPE", row) == "ELLIPSE")
-								{
-									CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_2", row));
-									CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_2", row));
-									rectellipse = true;
-								}
-								if(CleanElementAperture.Get_s("APERTYPE", row) == "RECTANGLE")
-								{
-									CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_2", row));
-									CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_2", row));
-								}
-								if(CleanElementAperture.Get_s("APERTYPE", row) == "OCTAGON")
-								{
-									CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
-									CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_2", row));
-									CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_3", row));
-									CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_4", row));
-									octagon = true;
-								}
+								CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_2", row));
+								CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_3", row));
+								CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_4", row));
+								rectellipse = true;
 							}
-							if(rectellipse && octagon)
+							if(CleanElementAperture.Get_s("APERTYPE", row) == "CIRCLE")
 							{
-								cerr
-									<<
-									"Attempting to interpolate incompatible aperture types (RECTELLIPSE and OCTAGON)... exiting simulation."
-									<< endl;
-								exit(EXIT_FAILURE);
+								CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_1", row));
+								rectellipse = true;
 							}
-							aperture = InterpolatorFactory::GetInstance(CleanElementAperture);
-							(*comp)->SetAperture(aperture);
-							++noConfiguredApertures;
-							break;
+							if(CleanElementAperture.Get_s("APERTYPE", row) == "ELLIPSE")
+							{
+								CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_2", row));
+								CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_2", row));
+								rectellipse = true;
+							}
+							if(CleanElementAperture.Get_s("APERTYPE", row) == "RECTANGLE")
+							{
+								CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_2", row));
+								CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_2", row));
+							}
+							if(CleanElementAperture.Get_s("APERTYPE", row) == "OCTAGON")
+							{
+								CleanElementAperture.Set("APER_1", row, CleanElementAperture.Get_d("APER_1", row));
+								CleanElementAperture.Set("APER_2", row, CleanElementAperture.Get_d("APER_2", row));
+								CleanElementAperture.Set("APER_3", row, CleanElementAperture.Get_d("APER_3", row));
+								CleanElementAperture.Set("APER_4", row, CleanElementAperture.Get_d("APER_4", row));
+								octagon = true;
+							}
 						}
-					}
-					else
-					{
-						DataTableRowIterator itr = CleanElementAperture.begin();
-						aperture = ApertureFactory::GetInstance(*itr);
-						(*comp)->SetAperture(aperture);
+						if(rectellipse && octagon)
+						{
+							cerr
+								<<
+								"Attempting to interpolate incompatible aperture types (RECTELLIPSE and OCTAGON)... exiting simulation."
+								<< endl;
+							exit(EXIT_FAILURE);
+						}
+						aperture = InterpolatorFactory::GetInstance(CleanElementAperture);
+						comp->SetAperture(aperture);
 						++noConfiguredApertures;
 						break;
 					}
 				}
+				else
+				{
+					DataTableRowIterator itr = CleanElementAperture.begin();
+					aperture = ApertureFactory::GetInstance(*itr);
+					comp->SetAperture(aperture);
+					++noConfiguredApertures;
+					break;
+				}
+				this_ap++;
 			}
 		}
 		if(logFlag)
 		{
-			*log << setw(25) << left << (*comp)->GetName();
-			*log << setw(14) << left << (*comp)->GetType();
-			*log << setw(10) << left << (*comp)->GetLength();
-			*log << setw(10) << left << (*comp)->GetComponentLatticePosition();
-			if((*comp)->GetAperture() != nullptr)
+			*log << setw(25) << left << comp->GetName();
+			*log << setw(14) << left << comp->GetType();
+			*log << setw(10) << left << comp->GetLength();
+			*log << setw(10) << left << comp->GetComponentLatticePosition();
+			if(comp->GetAperture() != nullptr)
 			{
-				(*comp)->GetAperture()->printout(*log);
+				comp->GetAperture()->printout(*log);
 			}
 			*log << endl;
 		}
